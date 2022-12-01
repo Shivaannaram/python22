7fc59b65ae72c2186abe9ee9fa257d6965c6291b
Shiva <shivaannaram000@gmail.com>
2022-12-01 18:44:38 +0530

    added operators file

---
+++ Eg6.py
@@ -0,0 +1,16 @@
+def ope(a,b):
+    sum=a+b
+    print("sum of two numbers is:",sum)
+
+    dif=a-b
+    print("deifference of two numbers is:",dif)
+
+    mul=a*b
+    print("multiplication of two numbers is:",mul)\
+
+    div=a/b
+    print("divison of two numbers is:", div)
+
+a=int(input("enter a number"))
+b=int(input("enter b number"))
+ope(a,b)
