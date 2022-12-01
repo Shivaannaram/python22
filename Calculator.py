788f9787127c66a5ea46b79044dcef85d39b22b3
Shiva <shivaannaram000@gmail.com>
2022-12-01 18:34:05 +0530

    added a python file

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+#def Cal(a):
+#    mul=a*i
+#    print(a, "*", i, "=", mul)
+#
+#for i in range(10):
+#    Cal(10)
+#
+#def max(x,y,z):
+# if x>y>z:
+ #       return x
+  #  elif y>z:
+   #     return y
+    #else:
+     #   return z
+#a=int(input("enter First Number"))
+#b=int(input("enter second number"))
+#c=int(input("enter Third number"))
+#print("max number",max(a,b,c))
+
+
+
+
+
+
